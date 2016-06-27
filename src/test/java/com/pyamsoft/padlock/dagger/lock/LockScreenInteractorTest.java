3e6933d058f05f00fc9d5b270ce9b99a252a5f1e
pyamsoft <pyam.soft@gmail.com>
2016-06-27 16:26:56 -0700

    Do not finalize Delegate methods so that they are fakeable.
    Add interactor test for unlock

--- src/test/java/com/pyamsoft/padlock/dagger/lock/LockScreenInteractorTest.java
+++ src/test/java/com/pyamsoft/padlock/dagger/lock/LockScreenInteractorTest.java
@@ -22,9 +22,11 @@ import android.support.annotation.NonNull;
 import com.pyamsoft.padlock.BuildConfig;
 import com.pyamsoft.padlock.PadLock;
 import com.pyamsoft.padlock.PadLockPreferences;
-import com.pyamsoft.padlock.app.base.PackageManagerWrapper;
+import com.pyamsoft.padlock.app.sql.PadLockDB;
 import com.pyamsoft.padlock.dagger.base.PackageManagerWrapperImpl;
 import com.pyamsoft.padlock.dagger.db.DBInteractor;
+import com.pyamsoft.padlock.model.sql.PadLockEntry;
+import com.pyamsoft.padlock.model.sql.TestPadLockEntry;
 import java.util.Collections;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -33,28 +35,43 @@ import org.mockito.Mockito;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import rx.Observable;
+import rx.Subscriber;
 import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 23, application = PadLock.class)
 public class LockScreenInteractorTest {
 
-  @CheckResult @NonNull public static LockScreenInteractor getLockScreenInteractor() {
-    final Context context = RuntimeEnvironment.application.getApplicationContext();
-    final DBInteractor mockDBInteractor = Mockito.mock(DBInteractor.class);
-    final PadLockPreferences mockPreferences = Mockito.mock(PadLockPreferences.class);
-    final PackageManagerWrapper packageManagerWrapper = new PackageManagerWrapperImpl(context);
-    final MasterPinInteractor mockMasterPinInteractor = Mockito.mock(MasterPinInteractor.class);
+  private Context context;
+  private DBInteractor mockDBInteractor;
+  private PadLockPreferences mockPreferences;
+  private PackageManagerWrapperImpl packageManagerWrapper;
+  private MasterPinInteractor mockMasterPinInteractor;
+
+  void reinit() {
+    PadLockDB.setDelegate(null);
+    context = RuntimeEnvironment.application.getApplicationContext();
+    mockDBInteractor = Mockito.mock(DBInteractor.class);
+    mockPreferences = Mockito.mock(PadLockPreferences.class);
+    packageManagerWrapper = new PackageManagerWrapperImpl(context);
+    mockMasterPinInteractor = Mockito.mock(MasterPinInteractor.class);
+  }
+
+  @CheckResult @NonNull public LockScreenInteractor getLockScreenInteractor() {
     return new LockScreenInteractorImpl(context, mockPreferences, mockDBInteractor,
         mockMasterPinInteractor, packageManagerWrapper);
   }
 
   @Test public void test_ignoreTime() {
+    reinit();
     final LockScreenInteractor interactor = getLockScreenInteractor();
     Assert.assertEquals(0L, interactor.getDefaultIgnoreTime().toBlocking().first().longValue());
   }
 
   @Test public void test_displayName() {
+    reinit();
     final LockScreenInteractor interactor = getLockScreenInteractor();
 
     // Robolectric returns the packageName
@@ -63,5 +80,80 @@ public class LockScreenInteractorTest {
     interactor.getDisplayName(packageName).subscribe(testSubscriber);
     testSubscriber.assertNoErrors();
     testSubscriber.assertReceivedOnNext(Collections.singletonList(packageName + ".PadLock"));
+  }
+
+  void init_test_unlock() {
+    reinit();
+    // Create a fake delegate
+    final PadLockDB.Delegate delegate = new PadLockDB.Delegate(context, Schedulers.immediate()) {
+      @NonNull @Override
+      public Observable<PadLockEntry> queryWithPackageActivityName(@NonNull String packageName,
+          @NonNull String activityName) {
+        return Observable.defer(() -> Observable.create(new Observable.OnSubscribe<PadLockEntry>() {
+          @Override public void call(Subscriber<? super PadLockEntry> subscriber) {
+            //System.out.println(
+            //    "FAKE queryWithPackageActivityName(  _packageName_ , _activityName_ )");
+            subscriber.onStart();
+            subscriber.onNext(TestPadLockEntry.testEntry());
+            subscriber.onCompleted();
+          }
+        }));
+      }
+    };
+    PadLockDB.setDelegate(delegate);
+
+    // Mock preference
+    Mockito.when(mockPreferences.getIgnoreTimes())
+        .thenReturn(new long[] { 0, 1, 5, 10, 15, 20, 30, 45, 60 });
+  }
+
+  @Test public void test_unlockFail() {
+    init_test_unlock();
+    String packageName = "com.pyamsoft.padlock";
+    String activityName = packageName + ".app.main.MainActivity";
+
+    final LockScreenInteractor interactor = getLockScreenInteractor();
+
+    // Mock master pin methods
+    Mockito.when(mockMasterPinInteractor.getMasterPin())
+        .thenReturn(Observable.create(new Observable.OnSubscribe<String>() {
+          @Override public void call(Subscriber<? super String> subscriber) {
+            //System.out.println("MOCK getMasterPin()");
+            subscriber.onStart();
+            subscriber.onNext(interactor.encodeSHA256("BOB").toBlocking().first());
+            subscriber.onCompleted();
+          }
+        }));
+
+    interactor.unlockEntry(packageName, activityName, "", false, 0).subscribe(result -> {
+      //System.out.println("Result for interactor unlockEntry should be false");
+      Assert.assertNotNull(result);
+      Assert.assertFalse(result);
+    }, Throwable::printStackTrace);
+  }
+
+  @Test public void test_unlockSuccess() {
+    init_test_unlock();
+    String packageName = "com.pyamsoft.padlock";
+    String activityName = packageName + ".app.main.MainActivity";
+
+    final LockScreenInteractor interactor = getLockScreenInteractor();
+
+    // Mock master pin methods
+    Mockito.when(mockMasterPinInteractor.getMasterPin())
+        .thenReturn(Observable.create(new Observable.OnSubscribe<String>() {
+          @Override public void call(Subscriber<? super String> subscriber) {
+            //System.out.println("MOCK getMasterPin()");
+            subscriber.onStart();
+            subscriber.onNext(interactor.encodeSHA256("BOB").toBlocking().first());
+            subscriber.onCompleted();
+          }
+        }));
+
+    interactor.unlockEntry(packageName, activityName, "BOB", false, 0).subscribe(result -> {
+      //System.out.println("Result for interactor unlockEntry should be true");
+      Assert.assertNotNull(result);
+      Assert.assertTrue(result);
+    }, Throwable::printStackTrace);
   }
 }
