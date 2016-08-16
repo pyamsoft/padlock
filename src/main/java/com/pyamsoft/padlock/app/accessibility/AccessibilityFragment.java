47658ae4a6d0d7d2a3e0b88120eeb6360c1dc218
pyamsoft <pyam.soft@gmail.com>
2016-08-15 22:45:05 -0700

    Unbinder got removed accidentally

--- src/main/java/com/pyamsoft/padlock/app/accessibility/AccessibilityFragment.java
+++ src/main/java/com/pyamsoft/padlock/app/accessibility/AccessibilityFragment.java
@@ -26,6 +26,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import com.pyamsoft.padlock.R;
 import com.pyamsoft.pydroid.base.fragment.ActionBarFragment;
 
@@ -35,11 +37,19 @@ public class AccessibilityFragment extends ActionBarFragment {
   @NonNull private final Intent accessibilityServiceIntent =
       new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
   @BindView(R.id.main_service_button) Button serviceButton;
+  private Unbinder unbinder;
 
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_accessibility_ask, container, false);
+    final View view = inflater.inflate(R.layout.fragment_accessibility_ask, container, false);
+    unbinder = ButterKnife.bind(this, view);
+    return view;
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    unbinder.unbind();
   }
 
   @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
