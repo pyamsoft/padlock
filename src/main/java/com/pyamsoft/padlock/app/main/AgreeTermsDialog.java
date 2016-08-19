2cdb7872af1a79e76549e4717457a53ab7961651
pyamsoft <pyam.soft@gmail.com>
2016-08-18 21:29:36 -0700

    Update to common pydroid rxbus

--- src/main/java/com/pyamsoft/padlock/app/main/AgreeTermsDialog.java
+++ src/main/java/com/pyamsoft/padlock/app/main/AgreeTermsDialog.java
@@ -23,8 +23,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
-import com.pyamsoft.padlock.model.RxBus;
 import com.pyamsoft.padlock.model.event.AgreeTermsEvent;
+import com.pyamsoft.pydroid.tool.RxBus;
 
 public class AgreeTermsDialog extends DialogFragment {
 
