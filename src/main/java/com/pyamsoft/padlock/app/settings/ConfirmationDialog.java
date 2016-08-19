34c23095aa0e44dcdea780d99de34066c2b09448
pyamsoft <pyam.soft@gmail.com>
2016-08-18 21:29:36 -0700

    Update to common pydroid rxbus

--- src/main/java/com/pyamsoft/padlock/app/settings/ConfirmationDialog.java
+++ src/main/java/com/pyamsoft/padlock/app/settings/ConfirmationDialog.java
@@ -23,8 +23,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
-import com.pyamsoft.padlock.model.RxBus;
 import com.pyamsoft.padlock.model.event.ConfirmationEvent;
+import com.pyamsoft.pydroid.tool.RxBus;
 
 public class ConfirmationDialog extends DialogFragment {
   @NonNull private static final String WHICH = "which_type";
