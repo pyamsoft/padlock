d169614b019dc15449ae73b0bac43c999097f621
pyamsoft <pyam.soft@gmail.com>
2016-09-08 10:03:39 -0700

    Move sql database out of app layer

--- src/main/java/com/pyamsoft/padlock/dagger/list/LockListInteractorImpl.java
+++ src/main/java/com/pyamsoft/padlock/dagger/list/LockListInteractorImpl.java
@@ -22,7 +22,7 @@ import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 import com.pyamsoft.padlock.PadLockPreferences;
-import com.pyamsoft.padlock.app.sql.PadLockDB;
+import com.pyamsoft.padlock.dagger.sql.PadLockDB;
 import com.pyamsoft.padlock.app.wrapper.PackageManagerWrapper;
 import com.pyamsoft.padlock.model.AppEntry;
 import com.pyamsoft.padlock.model.sql.PadLockEntry;
--- src/main/java/com/pyamsoft/padlock/dagger/sql/PadLockDB.java
+++ src/main/java/com/pyamsoft/padlock/dagger/sql/PadLockDB.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.pyamsoft.padlock.app.sql;
+package com.pyamsoft.padlock.dagger.sql;
 
 import android.content.Context;
 import android.support.annotation.CheckResult;
