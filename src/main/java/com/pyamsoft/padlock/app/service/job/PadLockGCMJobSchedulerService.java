30e25b7e677e6cdbce6d240e01d55d24a0036f4b
pyamsoft <pyam.soft@gmail.com>
2016-09-14 18:36:09 -0700

    Remove unused annotation

--- src/main/java/com/pyamsoft/padlock/app/service/job/PadLockGCMJobSchedulerService.java
+++ src/main/java/com/pyamsoft/padlock/app/service/job/PadLockGCMJobSchedulerService.java
@@ -16,7 +16,6 @@
 
 package com.pyamsoft.padlock.app.service.job;
 
-import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import com.birbit.android.jobqueue.JobManager;
 import com.birbit.android.jobqueue.scheduling.GcmJobSchedulerService;
@@ -24,8 +23,7 @@ import com.pyamsoft.padlock.PadLock;
 import com.pyamsoft.padlock.dagger.wrapper.JobSchedulerCompat;
 import javax.inject.Inject;
 
-@SuppressLint("Registered") public class PadLockGCMJobSchedulerService
-    extends GcmJobSchedulerService {
+public class PadLockGCMJobSchedulerService extends GcmJobSchedulerService {
 
   @Inject JobSchedulerCompat jobSchedulerCompat;
 
