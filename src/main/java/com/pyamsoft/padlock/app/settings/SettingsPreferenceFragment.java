d1978179ea89adfea86de89d87fd8d292de5571c
pyamsoft <pyam.soft@gmail.com>
2016-09-22 00:48:54 -0700

    Update to latest pydroid and remove obsolete android-apt

--- src/main/java/com/pyamsoft/padlock/app/settings/SettingsPreferenceFragment.java
+++ src/main/java/com/pyamsoft/padlock/app/settings/SettingsPreferenceFragment.java
@@ -28,10 +28,9 @@ import com.pyamsoft.padlock.R;
 import com.pyamsoft.padlock.app.service.PadLockService;
 import com.pyamsoft.padlock.bus.MainBus;
 import com.pyamsoft.padlock.model.event.RefreshEvent;
-import com.pyamsoft.pydroid.app.PersistLoader;
 import com.pyamsoft.pydroid.about.AboutLibrariesFragment;
+import com.pyamsoft.pydroid.app.PersistLoader;
 import com.pyamsoft.pydroid.app.fragment.ActionBarSettingsPreferenceFragment;
-import com.pyamsoft.pydroid.about.Licenses;
 import com.pyamsoft.pydroid.util.AppUtil;
 import com.pyamsoft.pydroid.util.PersistentCache;
 import timber.log.Timber;
@@ -87,12 +86,7 @@ public class SettingsPreferenceFragment extends ActionBarSettingsPreferenceFragm
     final Preference showAboutLicenses = findPreference(getString(R.string.about_license_key));
     showAboutLicenses.setOnPreferenceClickListener(
         preference -> showAboutLicensesFragment(R.id.main_view_container,
-            AboutLibrariesFragment.Styling.LIGHT, Licenses.Id.ANDROID, Licenses.Id.ANDROID_SUPPORT,
-            Licenses.Id.PYDROID, Licenses.Id.GOOGLE_PLAY_SERVICES, Licenses.Id.ANDROID_CHECKOUT,
-            Licenses.Id.AUTO_VALUE, Licenses.Id.BUTTERKNIFE, Licenses.Id.DAGGER,
-            Licenses.Id.FAST_ADAPTER, Licenses.Id.FIREBASE, Licenses.Id.LEAK_CANARY,
-            Licenses.Id.RETROFIT2, Licenses.Id.SQLBRITE, Licenses.Id.SQLDELIGHT,
-            Licenses.Id.ANDROID_PRIORITY_JOBQUEUE, Licenses.Id.RXANDROID, Licenses.Id.RXJAVA));
+            AboutLibrariesFragment.Styling.LIGHT));
 
     final Preference checkVersion = findPreference(getString(R.string.check_version_key));
     checkVersion.setOnPreferenceClickListener(preference -> checkForUpdate());
