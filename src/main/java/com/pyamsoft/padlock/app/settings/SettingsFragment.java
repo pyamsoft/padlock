bd35cda67a378697bd36e0fe32787a8454f0272c
pyamsoft <pyam.soft@gmail.com>
2016-09-21 20:21:49 -0700

    Update to latest pydroid

--- src/main/java/com/pyamsoft/padlock/app/settings/SettingsFragment.java
+++ src/main/java/com/pyamsoft/padlock/app/settings/SettingsFragment.java
@@ -27,7 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.pyamsoft.padlock.R;
 import com.pyamsoft.pydroid.util.CircularRevealFragmentUtil;
-import com.pyamsoft.pydroid.ActionBarFragment;
+import com.pyamsoft.pydroid.app.fragment.ActionBarFragment;
 
 public class SettingsFragment extends ActionBarFragment {
 
