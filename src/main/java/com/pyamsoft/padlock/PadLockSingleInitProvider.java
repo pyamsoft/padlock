9dddee97df47289920793149f7fe67017b32dccd
pyamsoft <pyam.soft@gmail.com>
2016-10-26 12:13:56 -0700

    Update application into single init, and list module

--- src/main/java/com/pyamsoft/padlock/PadLockSingleInitProvider.java
+++ src/main/java/com/pyamsoft/padlock/PadLockSingleInitProvider.java
@@ -16,7 +16,70 @@
 
 package com.pyamsoft.padlock;
 
+import android.content.Context;
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.pyamsoft.padlock.app.receiver.ApplicationInstallReceiver;
+import com.pyamsoft.padlock.dagger.PadLockComponent;
+import com.pyamsoft.padlock.dagger.PadLockModule;
+import com.pyamsoft.pydroid.IPYDroidApp;
 import com.pyamsoft.pydroid.SingleInitContentProvider;
+import com.pyamsoft.pydroid.about.Licenses;
 
-public class PadLockSingleInitProvider extends SingleInitContentProvider {
+public class PadLockSingleInitProvider extends SingleInitContentProvider
+    implements IPYDroidApp<PadLockComponent> {
+
+  @Nullable private static volatile PadLockSingleInitProvider instance = null;
+  @Nullable private PadLockComponent component;
+
+  @NonNull @CheckResult public static PadLockSingleInitProvider get() {
+    if (instance == null) {
+      throw new NullPointerException("Instance is NULL");
+    }
+
+    //noinspection ConstantConditions
+    return instance;
+  }
+
+  private static void setInstance(@NonNull PadLockSingleInitProvider instance) {
+    PadLockSingleInitProvider.instance = instance;
+  }
+
+  @Override protected void onFirstCreate(@NonNull Context context) {
+    super.onFirstCreate(context);
+    component = DaggerPadLockComponent.builder().padLockModule(new PadLockModule(context)).build();
+  }
+
+  @Override protected void onInstanceCreated(@NonNull Context context) {
+    setInstance(this);
+    final PadLockComponent comp = provideComponent();
+    final ApplicationInstallReceiver receiver = comp.provideApplicationInstallReceiver();
+    final PadLockPreferences preferences = comp.providePreferences();
+    if (preferences.isInstallListenerEnabled()) {
+      receiver.register();
+    } else {
+      receiver.unregister();
+    }
+  }
+
+  @Nullable @Override public String provideGoogleOpenSourceLicenses(@NonNull Context context) {
+    return GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(context);
+  }
+
+  @Override public void insertCustomLicensesIntoMap() {
+    Licenses.create("Android Priority Job Queue",
+        "https://github.com/yigit/android-priority-jobqueue", "licenses/androidpriorityjobqueue");
+    Licenses.create("SQLBrite", "https://github.com/square/sqlbrite", "licenses/sqlbrite");
+    Licenses.create("SQLDelight", "https://github.com/square/sqldelight", "licenses/sqldelight");
+  }
+
+  @NonNull @Override public PadLockComponent provideComponent() {
+    if (component == null) {
+      throw new NullPointerException("Component is NULL");
+    }
+
+    return component;
+  }
 }
