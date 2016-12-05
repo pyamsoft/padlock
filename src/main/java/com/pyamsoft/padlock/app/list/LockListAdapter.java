46942ace1e244890c6b6daa52b0d3d757ff00be9
pyamsoft <pyam.soft@gmail.com>
2016-12-04 16:01:35 -0800

    Instead of holding references to activity in List items, bind on clicks in the Fragment itself
    Should stop the memory leak on rotation in some cases

--- src/main/java/com/pyamsoft/padlock/app/list/LockListAdapter.java
+++ src/main/java/com/pyamsoft/padlock/app/list/LockListAdapter.java
@@ -24,16 +24,14 @@ class LockListAdapter extends FastItemAdapter<LockListItem> implements LockListD
   @Override public void onDatabaseEntryCreated(int position) {
     final LockListItem oldItem = getItem(position);
     final LockListItem newItem =
-        new LockListItem(AppEntry.builder(oldItem.getEntry()).locked(true).build(),
-            oldItem.getRequestListener(), oldItem.getModifyListener());
+        new LockListItem(AppEntry.builder(oldItem.getEntry()).locked(true).build());
     set(position, newItem);
   }
 
   @Override public void onDatabaseEntryDeleted(int position) {
     final LockListItem oldItem = getItem(position);
     final LockListItem newItem =
-        new LockListItem(AppEntry.builder(oldItem.getEntry()).locked(false).build(),
-            oldItem.getRequestListener(), oldItem.getModifyListener());
+        new LockListItem(AppEntry.builder(oldItem.getEntry()).locked(false).build());
     set(position, newItem);
   }
 }
