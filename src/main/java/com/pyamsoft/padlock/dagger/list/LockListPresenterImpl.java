368fcf343e240e1903b00ce45078d946596ce4d7
pyamsoft <pyam.soft@gmail.com>
2016-12-14 13:33:32 -0800

    apply same logic to the LockList presenter to slightly speed up processing

--- src/main/java/com/pyamsoft/padlock/dagger/list/LockListPresenterImpl.java
+++ src/main/java/com/pyamsoft/padlock/dagger/list/LockListPresenterImpl.java
@@ -16,6 +16,7 @@
 
 package com.pyamsoft.padlock.dagger.list;
 
+import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
@@ -25,6 +26,8 @@ import com.pyamsoft.padlock.dagger.service.LockServiceStateInteractor;
 import com.pyamsoft.padlock.model.AppEntry;
 import com.pyamsoft.padlock.model.sql.PadLockEntry;
 import com.pyamsoft.pydroidrx.SubscriptionHelper;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -123,20 +126,35 @@ class LockListPresenterImpl extends LockCommonPresenterImpl<LockListPresenter.Lo
                   }
                 }))
         .filter(s -> s != null)
-        .withLatestFrom(lockListInteractor.getAppEntryList(), (packageName, padLockEntries) -> {
-          PadLockEntry.AllEntries found = null;
-          for (final PadLockEntry.AllEntries padLockEntry : padLockEntries) {
-            if (padLockEntry.packageName().equals(packageName) && padLockEntry.activityName()
-                .equals(PadLockEntry.PACKAGE_ACTIVITY_NAME)) {
-              found = padLockEntry;
-              break;
-            }
-          }
+        .toList()
+        .withLatestFrom(lockListInteractor.getAppEntryList()
+                .flatMap(Observable::from)
+                .toSortedList((allEntries, allEntries2) -> allEntries.packageName()
+                    .compareToIgnoreCase(allEntries2.packageName())),
+            (packageNames, padLockEntries) -> {
+              final List<Pair<String, Boolean>> lockPairs = new ArrayList<>();
+              int start = 0;
+              int end = packageNames.size() - 1;
 
-          final boolean locked = found != null;
-          Timber.d("New pair: %s %s", packageName, locked);
-          return new Pair<>(packageName, locked);
-        })
+              while (start <= end) {
+                // Find entry to compare against
+                final Pair<String, Boolean> entry1 =
+                    findAppEntry(packageNames, padLockEntries, start);
+                lockPairs.add(entry1);
+
+                if (start != end) {
+                  final Pair<String, Boolean> entry2 =
+                      findAppEntry(packageNames, padLockEntries, end);
+                  lockPairs.add(entry2);
+                }
+
+                ++start;
+                --end;
+              }
+
+              return lockPairs;
+            })
+        .flatMap(Observable::from)
         .flatMap(pair -> lockListInteractor.createFromPackageInfo(pair.first, pair.second))
         .sorted((entry, entry2) -> entry.name().compareToIgnoreCase(entry2.name()))
         .map(appEntry -> {
@@ -163,6 +181,64 @@ class LockListPresenterImpl extends LockCommonPresenterImpl<LockListPresenter.Lo
               getView(LockList::onListPopulated);
               SubscriptionHelper.unsubscribe(populateListSubscription);
             });
+  }
+
+  @SuppressWarnings("WeakerAccess") @CheckResult @NonNull Pair<String, Boolean> findAppEntry(
+      @NonNull List<String> packageNames, @NonNull List<PadLockEntry.AllEntries> padLockEntries,
+      int index) {
+    final String packageName = packageNames.get(index);
+    final PadLockEntry.AllEntries foundEntry = findMatchingEntry(padLockEntries, packageName);
+    return new Pair<>(packageName, foundEntry != null);
+  }
+
+  @SuppressWarnings("WeakerAccess") @CheckResult @Nullable
+  PadLockEntry.AllEntries findMatchingEntry(@NonNull List<PadLockEntry.AllEntries> padLockEntries,
+      @NonNull String packageName) {
+    if (padLockEntries.isEmpty()) {
+      Timber.d("Did not find entry for %s", packageName);
+      return null;
+    }
+
+    // Select a pivot point
+    final int middle = padLockEntries.size() / 2;
+    final PadLockEntry.AllEntries pivotPoint = padLockEntries.get(middle);
+
+    // Compare to pivot
+    int start;
+    int end;
+    PadLockEntry.AllEntries foundEntry = null;
+    if (pivotPoint.packageName().equals(packageName)) {
+      // We are the pivot
+      foundEntry = pivotPoint;
+      start = 0;
+      end = -1;
+    } else if (packageName.compareToIgnoreCase(pivotPoint.packageName()) < 0) {
+      //  We are before the pivot point
+      start = 0;
+      end = middle - 1;
+    } else {
+      // We are after the pivot point
+      start = middle + 1;
+      end = padLockEntries.size() - 1;
+    }
+
+    while (start <= end) {
+      final PadLockEntry.AllEntries checkEntry1 = padLockEntries.get(start++);
+      final PadLockEntry.AllEntries checkEntry2 = padLockEntries.get(end--);
+      if (packageName.equals(checkEntry1.packageName())) {
+        foundEntry = checkEntry1;
+        break;
+      } else if (packageName.equals(checkEntry2.packageName())) {
+        foundEntry = checkEntry2;
+        break;
+      }
+    }
+
+    if (foundEntry != null) {
+      padLockEntries.remove(foundEntry);
+    }
+
+    return foundEntry;
   }
 
   @Override public void setFABStateFromPreference() {
