3a42de8511b7a00a806c0b22e008448dbe152644
pyamsoft <pyam.soft@gmail.com>
2016-12-14 13:20:28 -0800

    List processing optimization for slightly faster LockInfo loading

--- src/main/java/com/pyamsoft/padlock/dagger/list/LockInfoPresenterImpl.java
+++ src/main/java/com/pyamsoft/padlock/dagger/list/LockInfoPresenterImpl.java
@@ -16,6 +16,7 @@
 
 package com.pyamsoft.padlock.dagger.list;
 
+import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.pyamsoft.padlock.app.iconloader.AppIconLoaderPresenter;
@@ -24,6 +25,8 @@ import com.pyamsoft.padlock.model.ActivityEntry;
 import com.pyamsoft.padlock.model.LockState;
 import com.pyamsoft.padlock.model.sql.PadLockEntry;
 import com.pyamsoft.pydroidrx.SubscriptionHelper;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -81,7 +84,6 @@ class LockInfoPresenterImpl extends LockCommonPresenterImpl<LockInfoPresenter.Lo
   }
 
   @Override public void clearList() {
-    Timber.w("Clear activity entry cache");
     lockInfoInteractor.clearCache();
   }
 
@@ -95,30 +97,36 @@ class LockInfoPresenterImpl extends LockCommonPresenterImpl<LockInfoPresenter.Lo
     Timber.d("Populate list");
 
     final Observable<ActivityEntry> freshData = lockInfoInteractor.getPackageActivities(packageName)
-        .withLatestFrom(lockInfoInteractor.getActivityEntries(packageName),
-            (activityName, padLockEntries) -> {
-              PadLockEntry.WithPackageName foundEntry = null;
-              for (int i = 0; i < padLockEntries.size(); ++i) {
-                final PadLockEntry.WithPackageName entry = padLockEntries.get(i);
-                if (entry.activityName().equals(activityName)) {
-                  foundEntry = entry;
-                  break;
+        .toList()
+        .withLatestFrom(lockInfoInteractor.getActivityEntries(packageName)
+                .flatMap(Observable::from)
+                .toSortedList((withPackageName, withPackageName2) -> withPackageName.activityName()
+                    .compareToIgnoreCase(withPackageName2.activityName())),
+            (activityNames, padLockEntries) -> {
+              final List<ActivityEntry> activityEntries = new ArrayList<>();
+
+              int start = 0;
+              int end = activityNames.size() - 1;
+
+              while (start <= end) {
+                // Find entry to compare against
+                final ActivityEntry entry1 =
+                    findActivityEntry(activityNames, padLockEntries, start);
+                activityEntries.add(entry1);
+
+                if (start != end) {
+                  final ActivityEntry entry2 =
+                      findActivityEntry(activityNames, padLockEntries, end);
+                  activityEntries.add(entry2);
                 }
+
+                ++start;
+                --end;
               }
 
-              final LockState state;
-              if (foundEntry == null) {
-                state = LockState.DEFAULT;
-              } else {
-                if (foundEntry.whitelist()) {
-                  state = LockState.WHITELISTED;
-                } else {
-                  state = LockState.LOCKED;
-                }
-              }
-
-              return ActivityEntry.builder().name(activityName).lockState(state).build();
+              return activityEntries;
             })
+        .flatMap(Observable::from)
         .sorted((activityEntry, activityEntry2) -> {
           // Package names are all the same
           final String entry1Name = activityEntry.name();
@@ -175,6 +183,80 @@ class LockInfoPresenterImpl extends LockCommonPresenterImpl<LockInfoPresenter.Lo
           getView(LockInfoView::onListPopulated);
           SubscriptionHelper.unsubscribe(populateListSubscription);
         });
+  }
+
+  @SuppressWarnings("WeakerAccess") @CheckResult @NonNull ActivityEntry findActivityEntry(
+      @NonNull List<String> activityNames,
+      @NonNull List<PadLockEntry.WithPackageName> padLockEntries, int index) {
+    final String activityName = activityNames.get(index);
+    final PadLockEntry.WithPackageName foundEntry = findMatchingEntry(padLockEntries, activityName);
+    return createActivityEntry(activityName, foundEntry);
+  }
+
+  @SuppressWarnings("WeakerAccess") @CheckResult @Nullable
+  PadLockEntry.WithPackageName findMatchingEntry(
+      @NonNull List<PadLockEntry.WithPackageName> padLockEntries, @NonNull String activityName) {
+    if (padLockEntries.isEmpty()) {
+      Timber.d("Did not find entry for %s", activityName);
+      return null;
+    }
+
+    // Select a pivot point
+    final int middle = padLockEntries.size() / 2;
+    final PadLockEntry.WithPackageName pivotPoint = padLockEntries.get(middle);
+
+    // Compare to pivot
+    int start;
+    int end;
+    PadLockEntry.WithPackageName foundEntry = null;
+    if (pivotPoint.activityName().equals(activityName)) {
+      // We are the pivot
+      foundEntry = pivotPoint;
+      start = 0;
+      end = -1;
+    } else if (activityName.compareToIgnoreCase(pivotPoint.activityName()) < 0) {
+      //  We are before the pivot point
+      start = 0;
+      end = middle - 1;
+    } else {
+      // We are after the pivot point
+      start = middle + 1;
+      end = padLockEntries.size() - 1;
+    }
+
+    while (start <= end) {
+      final PadLockEntry.WithPackageName checkEntry1 = padLockEntries.get(start++);
+      final PadLockEntry.WithPackageName checkEntry2 = padLockEntries.get(end--);
+      if (activityName.equals(checkEntry1.activityName())) {
+        foundEntry = checkEntry1;
+        break;
+      } else if (activityName.equals(checkEntry2.activityName())) {
+        foundEntry = checkEntry2;
+        break;
+      }
+    }
+
+    if (foundEntry != null) {
+      padLockEntries.remove(foundEntry);
+    }
+
+    return foundEntry;
+  }
+
+  @SuppressWarnings("WeakerAccess") @CheckResult @NonNull ActivityEntry createActivityEntry(
+      @NonNull String name, @Nullable PadLockEntry.WithPackageName foundEntry) {
+    final LockState state;
+    if (foundEntry == null) {
+      state = LockState.DEFAULT;
+    } else {
+      if (foundEntry.whitelist()) {
+        state = LockState.WHITELISTED;
+      } else {
+        state = LockState.LOCKED;
+      }
+    }
+
+    return ActivityEntry.builder().name(name).lockState(state).build();
   }
 
   @Override
