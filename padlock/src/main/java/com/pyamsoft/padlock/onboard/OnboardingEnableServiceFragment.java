7e1970099c0f835a43dabe85d82699fa4d741d6d
pyamsoft <pyam.soft@gmail.com>
2017-01-13 12:33:14 -0800

    Add basic onbording UI
    3 simple pages
    First page greets user
    Second enables service
    Third accepts terms of use

---
+++ padlock/src/main/java/com/pyamsoft/padlock/onboard/OnboardingEnableServiceFragment.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.padlock.onboard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.pyamsoft.padlock.databinding.OnboardingEnableServiceBinding;
+
+public class OnboardingEnableServiceFragment extends Fragment {
+
+  private OnboardingEnableServiceBinding binding;
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = OnboardingEnableServiceBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    binding.unbind();
+  }
+}
