bf969991ddbafb17e2d1cba4e4687e556fe5fe37
pyamsoft <pyam.soft@gmail.com>
2017-01-20 16:41:05 -0800

    Add onboard image for list screen 2 and 3

--- padlock/src/main/java/com/pyamsoft/padlock/onboard/list/OnboardLockPackageFragment.java
+++ padlock/src/main/java/com/pyamsoft/padlock/onboard/list/OnboardLockPackageFragment.java
@@ -25,6 +25,6 @@ public class OnboardLockPackageFragment extends OnboardContentFragment {
   }
 
   @Override protected int getOnboardImage() {
-    return R.drawable.list_onboard_1;
+    return R.drawable.list_onboard_2;
   }
 }
