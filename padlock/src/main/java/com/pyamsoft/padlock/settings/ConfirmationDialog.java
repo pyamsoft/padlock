558f152960334494f55f1cd0caacf648291ab2f1
pyamsoft <pyam.soft@gmail.com>
2017-02-01 19:27:11 -0800

    Fix main activity launch, and settings clear

--- padlock/src/main/java/com/pyamsoft/padlock/settings/ConfirmationDialog.java
+++ padlock/src/main/java/com/pyamsoft/padlock/settings/ConfirmationDialog.java
@@ -16,10 +16,7 @@
 
 package com.pyamsoft.padlock.settings;
 
-import android.app.Activity;
-import android.app.ActivityManager;
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -28,9 +25,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import com.pyamsoft.padlock.PadLock;
-import com.pyamsoft.padlock.main.MainActivity;
-import com.pyamsoft.padlock.service.PadLockService;
-import timber.log.Timber;
 
 public class ConfirmationDialog extends DialogFragment {
   @NonNull private static final String WHICH = "which_type";
@@ -55,7 +49,6 @@ public class ConfirmationDialog extends DialogFragment {
         ? "Really clear entire database?\n\nYou will have to re-configure all locked applications again"
         : "Really clear all application settings?\n\nYou will have to manually restart the Accessibility Service component of PadLock")
         .setPositiveButton("Yes", (dialogInterface, i) -> {
-          dismiss();
           sendConfirmationEvent(which);
         })
         .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss())
@@ -67,30 +60,8 @@ public class ConfirmationDialog extends DialogFragment {
     final Fragment settingsPreferenceFragment =
         fragmentManager.findFragmentByTag(SettingsFragment.TAG);
     if (settingsPreferenceFragment instanceof SettingsFragment) {
-      ((SettingsFragment) settingsPreferenceFragment).getPresenter()
-          .processClearRequest(which, new SettingsPreferencePresenter.ClearCallback() {
-            @Override public void onClearAll() {
-              Timber.d("Everything is cleared, kill self");
-              try {
-                PadLockService.finish();
-              } catch (NullPointerException e) {
-                Timber.e(e, "Expected NPE when Service is NULL");
-              }
-              final ActivityManager activityManager =
-                  (ActivityManager) getActivity().getApplicationContext()
-                      .getSystemService(Context.ACTIVITY_SERVICE);
-              activityManager.clearApplicationUserData();
-            }
-
-            @Override public void onClearDatabase() {
-              final Activity activity = getActivity();
-              if (activity instanceof MainActivity) {
-                ((MainActivity) activity).onForceRefresh();
-              } else {
-                throw new ClassCastException("Activity is not MainActivity");
-              }
-            }
-          });
+      SettingsFragment settingsFragment = ((SettingsFragment) settingsPreferenceFragment);
+      settingsFragment.getPresenter().processClearRequest(which, settingsFragment);
     } else {
       throw new ClassCastException("Fragment is not SettingsPreferenceFragment");
     }
