c981752e0f7fe9fae3f1d84b23c097c96ae390f0
pyamsoft <pyam.soft@gmail.com>
2017-04-14 19:22:53 -0700

    Fix interactor reporting PACKAGE locked for individual Activity entries

--- padlock-list/src/main/java/com/pyamsoft/padlock/list/LockListInteractor.java
+++ padlock-list/src/main/java/com/pyamsoft/padlock/list/LockListInteractor.java
@@ -19,7 +19,6 @@ package com.pyamsoft.padlock.list;
 import android.content.pm.ApplicationInfo;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
 import com.pyamsoft.padlock.base.db.PadLockDB;
 import com.pyamsoft.padlock.base.db.PadLockEntry;
@@ -34,7 +33,9 @@ import io.reactivex.Single;
 import io.reactivex.functions.BiFunction;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import timber.log.Timber;
@@ -95,6 +96,7 @@ import timber.log.Timber;
           ApplicationInfo info = wrapper.item();
           return getActivityListForApplication(info).map(activityList -> {
             if (activityList.isEmpty()) {
+              Timber.w("Entry: %s has no activities, hide.", info.packageName);
               return "";
             } else {
               return info.packageName;
@@ -110,28 +112,29 @@ import timber.log.Timber;
           Collections.sort(padLockEntries,
               (o1, o2) -> o1.packageName().compareToIgnoreCase(o2.packageName()));
 
+          final List<PadLockEntry.AllEntries> copyEntries = new ArrayList<>(padLockEntries);
+          final List<String> copyNames = new ArrayList<>(packageNames);
           final List<Pair<String, Boolean>> lockPairs = new ArrayList<>();
-          int start = 0;
-          int end = packageNames.size() - 1;
-
-          while (start <= end) {
-            // Find entry to compare against
-            final Pair<String, Boolean> entry1 = findAppEntry(packageNames, padLockEntries, start);
-            lockPairs.add(entry1);
-
-            if (start != end) {
-              final Pair<String, Boolean> entry2 = findAppEntry(packageNames, padLockEntries, end);
-              lockPairs.add(entry2);
+          for (final String packageName : copyNames) {
+            final Set<PadLockEntry.AllEntries> removeEntries = new HashSet<>();
+            PadLockEntry.AllEntries foundEntry = null;
+            for (final PadLockEntry.AllEntries entry : copyEntries) {
+              if (entry.packageName().equals(packageName)) {
+                removeEntries.add(entry);
+                if (entry.activityName().equals(PadLockEntry.PACKAGE_ACTIVITY_NAME)) {
+                  foundEntry = entry;
+                }
+              }
             }
 
-            ++start;
-            --end;
+            copyEntries.removeAll(removeEntries);
+            lockPairs.add(
+                new Pair<>(packageName, foundEntry == null ? Boolean.FALSE : Boolean.TRUE));
           }
 
           return lockPairs;
         })
-        .toObservable()
-        .flatMap(Observable::fromIterable)
+        .flatMapObservable(Observable::fromIterable)
         .flatMapMaybe(pair -> createFromPackageInfo(pair.first, pair.second))
         .toSortedList((entry, entry2) -> entry.name().compareToIgnoreCase(entry2.name()));
   }
@@ -165,62 +168,6 @@ import timber.log.Timber;
   @SuppressWarnings("WeakerAccess") @CheckResult @NonNull
   Single<List<PadLockEntry.AllEntries>> getAppEntryList() {
     return padLockDB.queryAll();
-  }
-
-  @SuppressWarnings("WeakerAccess") @CheckResult @NonNull Pair<String, Boolean> findAppEntry(
-      @NonNull List<String> packageNames, @NonNull List<PadLockEntry.AllEntries> padLockEntries,
-      int index) {
-    final String packageName = packageNames.get(index);
-    final PadLockEntry.AllEntries foundEntry = findMatchingEntry(padLockEntries, packageName);
-    return new Pair<>(packageName, foundEntry != null);
-  }
-
-  @CheckResult @Nullable private PadLockEntry.AllEntries findMatchingEntry(
-      @NonNull List<PadLockEntry.AllEntries> padLockEntries, @NonNull String packageName) {
-    if (padLockEntries.isEmpty()) {
-      return null;
-    }
-
-    // Select a pivot point
-    final int middle = padLockEntries.size() / 2;
-    final PadLockEntry.AllEntries pivotPoint = padLockEntries.get(middle);
-
-    // Compare to pivot
-    int start;
-    int end;
-    PadLockEntry.AllEntries foundEntry = null;
-    if (pivotPoint.packageName().equals(packageName)) {
-      // We are the pivot
-      foundEntry = pivotPoint;
-      start = 0;
-      end = -1;
-    } else if (packageName.compareToIgnoreCase(pivotPoint.packageName()) < 0) {
-      //  We are before the pivot point
-      start = 0;
-      end = middle - 1;
-    } else {
-      // We are after the pivot point
-      start = middle + 1;
-      end = padLockEntries.size() - 1;
-    }
-
-    while (start <= end) {
-      final PadLockEntry.AllEntries checkEntry1 = padLockEntries.get(start++);
-      final PadLockEntry.AllEntries checkEntry2 = padLockEntries.get(end--);
-      if (packageName.equals(checkEntry1.packageName())) {
-        foundEntry = checkEntry1;
-        break;
-      } else if (packageName.equals(checkEntry2.packageName())) {
-        foundEntry = checkEntry2;
-        break;
-      }
-    }
-
-    if (foundEntry != null) {
-      padLockEntries.remove(foundEntry);
-    }
-
-    return foundEntry;
   }
 
   /**
