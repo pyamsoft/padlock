d7e1d08e15597899dfaaec42daec61d489119335
pyamsoft <pyam.soft@gmail.com>
2017-07-22 12:10:49 -0700

    Update pydroid to fix optional nullable issue

---
+++ padlock-lock/src/main/java/com/pyamsoft/padlock/lock/common/LockTypePresenter.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.padlock.lock.common
+
+import com.pyamsoft.padlock.model.LockScreenType.TYPE_PATTERN
+import com.pyamsoft.padlock.model.LockScreenType.TYPE_TEXT
+import com.pyamsoft.pydroid.presenter.SchedulerPresenter
+import io.reactivex.Scheduler
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Named
+
+open class LockTypePresenter @Inject protected constructor(
+    private val interactor: LockTypeInteractor,
+    @Named("obs") obsScheduler: Scheduler,
+    @Named("sub") subScheduler: Scheduler) : SchedulerPresenter(obsScheduler, subScheduler) {
+
+  fun initializeLockScreenType(onTypePattern: () -> Unit, onTypeText: () -> Unit) {
+    disposeOnStop {
+      interactor.getLockScreenType()
+          .subscribeOn(backgroundScheduler)
+          .observeOn(foregroundScheduler)
+          .subscribe({
+            when (it) {
+              TYPE_PATTERN -> onTypePattern()
+              TYPE_TEXT -> onTypeText()
+            }
+          }, {
+            Timber.e(it, "Error initializing lock screen type")
+          })
+    }
+  }
+
+}
---
+++ padlock-lock/src/main/java/com/pyamsoft/padlock/lock/master/MasterPinInteractor.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.padlock.lock.master
+
+import android.support.annotation.CheckResult
+import com.pyamsoft.padlock.base.preference.MasterPinPreferences
+import com.pyamsoft.pydroid.helper.Optional
+import io.reactivex.Single
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton class MasterPinInteractor @Inject internal constructor(
+    private val preferences: MasterPinPreferences) {
+
+  @CheckResult fun getMasterPin(): Single<Optional<String>> {
+    return Single.fromCallable { Optional.ofNullable(preferences.masterPassword) }
+  }
+
+  fun setMasterPin(pin: String?) {
+    if (pin == null) {
+      preferences.clearMasterPassword()
+    } else {
+      preferences.setMasterPassword(pin)
+    }
+  }
+
+  @CheckResult fun getHint(): Single<Optional<String>> {
+    return Single.fromCallable { Optional.ofNullable(preferences.hint) }
+  }
+
+  fun setHint(hint: String?) {
+    if (hint == null) {
+      preferences.clearHint()
+    } else {
+      preferences.setHint(hint)
+    }
+  }
+}
