78be8d99f3de9b301bd0ca361c2ffd30a487d233
pyamsoft <pyam.soft@gmail.com>
2017-11-01 20:13:36 -0700

    If we whitelist an application, clear caches so that views update

--- padlock-lock/src/main/java/com/pyamsoft/padlock/lock/LockEntryInteractorCache.kt
+++ padlock-lock/src/main/java/com/pyamsoft/padlock/lock/LockEntryInteractorCache.kt
@@ -27,6 +27,8 @@ import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton internal class LockEntryInteractorCache @Inject internal constructor(
+    @param:Named("cache_lock_list") private val lockListCache: Cache,
+    @param:Named("cache_lock_info") private val lockInfoCache: Cache,
     @param:Named(
         "interactor_lock_entry") private val impl: LockEntryInteractor) : LockEntryInteractor, Cache {
 
@@ -44,9 +46,15 @@ import javax.inject.Singleton
   override fun getHint(): Single<String> = impl.getHint()
 
   override fun postUnlock(packageName: String, activityName: String, realName: String,
-      lockCode: String?, isSystem: Boolean, shouldExclude: Boolean, ignoreTime: Long): Completable =
-      impl.postUnlock(packageName, activityName, realName, lockCode, isSystem, shouldExclude,
-          ignoreTime)
+      lockCode: String?, isSystem: Boolean, whitelist: Boolean, ignoreTime: Long): Completable =
+      impl.postUnlock(packageName, activityName, realName, lockCode, isSystem, whitelist,
+          ignoreTime).doOnComplete {
+        if (whitelist) {
+          // Clear caches so that views update when we return to them
+          lockListCache.clearCache()
+          lockInfoCache.clearCache()
+        }
+      }
 
   override fun clearFailCount() = impl.clearFailCount()
 
