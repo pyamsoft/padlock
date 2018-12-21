c69f5e27ac2b07b0d6263ce642399809c7cafed3
pyamsoft <developer.pyamsoft@gmail.com>
2018-12-20 22:28:34 -0800

    Color lock info group background slightly darker

--- padlock/src/main/java/com/pyamsoft/padlock/list/LockInfoGroupViewImpl.kt
+++ padlock/src/main/java/com/pyamsoft/padlock/list/LockInfoGroupViewImpl.kt
@@ -1,13 +1,31 @@
 package com.pyamsoft.padlock.list
 
+import android.graphics.drawable.ColorDrawable
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.pyamsoft.padlock.R
 import com.pyamsoft.padlock.databinding.AdapterItemLockinfoGroupBinding
 import com.pyamsoft.padlock.model.list.ActivityEntry.Group
+import com.pyamsoft.pydroid.ui.theme.Theming
 import javax.inject.Inject
 
-internal class LockInfoGroupViewImpl @Inject internal constructor(itemView: View) : LockInfoGroupView {
+internal class LockInfoGroupViewImpl @Inject internal constructor(
+  itemView: View,
+  theming: Theming
+) : LockInfoGroupView {
 
   private val binding = AdapterItemLockinfoGroupBinding.bind(itemView)
+
+  init {
+    val color: Int
+    if (theming.isDarkTheme()) {
+      color = R.color.dark_lock_group_background
+    } else {
+      color = R.color.lock_group_background
+    }
+
+    binding.root.background = ColorDrawable(ContextCompat.getColor(itemView.context, color))
+  }
 
   override fun bind(
     model: Group,
